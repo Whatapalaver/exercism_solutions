93d1f4238b5b78c55d29c8507c58504121c505d6
whatapalaver <warriorwomenblog@gmail.com>
2019-05-03 13:04:16 +0100

    Refactor acronym solution

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,6 +1,6 @@
 class Acronym
   def self.abbreviate(phrase)
-    array = phrase.split(/\W+/)
-    array.map { |word| word[0].upcase }.join
+    array = phrase.scan(/\b\w/)
+    array.map { |word| word[0] }.join.upcase
   end
 end
