a9645fc8402f50c21e86c99f9b8cf0c39a0f8a9d
whatapalaver <warriorwomenblog@gmail.com>
2019-05-28 14:49:06 +0100

    Refactoring to utilise class methods

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,27 +1,38 @@
 class Grains
-  LOCATIONS = 1..64
+  LOCATIONS = (1..64).freeze
   MULTIPLIER = 2
 
-  class << self
-    def square(location)
-      unless valid?(location)
-        raise ArgumentError, "BoardLocationError: Must be between #{LOCATIONS.min} and #{LOCATIONS.max}"
-      end
-      position_total(location - 1)
+  def initialize(location)
+    @location = location
+  end
+
+  def self.square(location)
+    new(location).square
+  end
+
+  def self.total
+    new(LOCATIONS.min).total
+  end
+
+  def square
+    unless valid?(@location)
+      raise ArgumentError, "BoardLocationError: Must be between #{LOCATIONS.min} and #{LOCATIONS.max}"
     end
 
-    def total
-      position_total(LOCATIONS.max) - 1
-    end
+    position_total(@location - 1)
+  end
 
-    private
+  def total
+    position_total(LOCATIONS.max) - 1
+  end
 
-    def valid?(location)
-      LOCATIONS.include?(location)
-    end
+  private
 
-    def position_total(index)
-      MULTIPLIER.pow(index)
-    end
+  def valid?(location)
+    LOCATIONS.include?(location)
+  end
+
+  def position_total(index)
+    MULTIPLIER.pow(index)
   end
 end
