dec08128dfff4b8d6112f3141a8f4ed60496e816
whatapalaver <warriorwomenblog@gmail.com>
2019-05-28 16:04:55 +0100

    Class, private, public method reordering

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -2,14 +2,6 @@ class Grains
   LOCATIONS = (1..64).freeze
   MULTIPLIER = 2
 
-  def initialize(location)
-    unless valid?(location)
-      raise ArgumentError, "BoardLocationError: Must be between #{LOCATIONS.min} and #{LOCATIONS.max}"
-    end
-
-    @location = location
-  end
-
   def self.square(location)
     new(location).square
   end
@@ -18,15 +10,15 @@ class Grains
     new(LOCATIONS.min).total
   end
 
-  def square
-    position_total(@location - 1)
-  end
-
-  def total
-    position_total(LOCATIONS.max) - 1
-  end
-
   private
+
+  def initialize(location)
+    unless valid?(location)
+      raise ArgumentError, "BoardLocationError: Must be between #{LOCATIONS.min} and #{LOCATIONS.max}"
+    end
+
+    @location = location
+  end
 
   def valid?(location)
     LOCATIONS.include?(location)
@@ -34,5 +26,15 @@ class Grains
 
   def position_total(index)
     MULTIPLIER.pow(index)
+  end
+
+  public
+
+  def square
+    position_total(@location - 1)
+  end
+
+  def total
+    position_total(LOCATIONS.max) - 1
   end
 end
