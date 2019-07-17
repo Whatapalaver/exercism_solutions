cfd518de5a236333f244bde41377f327461ec2a9
whatapalaver <warriorwomenblog@gmail.com>
2019-07-17 14:40:48 +0100

    Refactor ruby anagram solution

--- ruby/anagram/anagram.rb
+++ ruby/anagram/anagram.rb
@@ -1,17 +1,22 @@
 class Anagram
 
   def initialize(word)
-    @downcased_word = word.downcase
-    @alphabetised_word = @downcased_word.chars.sort(&:casecmp).join
+    @anagram_chars = character_splitter(word)
   end
 
   def match(dictionary)
-    alphabetised_dictionary = dictionary.map { |word| word.downcase.chars.sort(&:casecmp).join }
-    matching_indeces = alphabetised_dictionary.each_index.select do |index|
-      alphabetised_dictionary[index] == @alphabetised_word
+    dictionary.select do |word|
+      valid_anagram?(character_splitter(word))
     end
-    temp_result_inc_direct_match = dictionary.values_at(*matching_indeces)
-    temp_result_inc_direct_match.delete_if { |word| word.downcase == @downcased_word}
   end
 
+  private
+
+  def valid_anagram?(character_array)
+    @anagram_chars.sort == character_array.sort && @anagram_chars != character_array
+  end
+
+  def character_splitter(word)
+    word.downcase.chars
+  end
 end
