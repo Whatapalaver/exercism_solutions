14620393a15e843ff9ca75d11f058fe036a414cd
whatapalaver <warriorwomenblog@gmail.com>
2019-07-18 10:27:45 +0100

    Refactoring to split temporary variable

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -4,10 +4,15 @@ class Phrase
   end
 
   def word_count
-    phrase_unquote = @phrase.gsub(/'\s|\s'/, ' ')
-    word_array = phrase_unquote.downcase.scan(/[\w']+/)
-    word_array.each_with_object(Hash.new(0)) do |word, hash|
+    phrase_unquote = @phrase.gsub(/['"]\s|\s['"]/, ' ')
+    split(phrase_unquote).each_with_object(Hash.new(0)) do |word, hash|
       hash[word] += 1
     end
+  end
+
+  private
+
+  def split(phrase)
+    phrase.downcase.scan(/[\w']+/)
   end
 end
