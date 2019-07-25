2c167bdcfb741ad346d1853ef0006a1b66238c27
Angela Wolff <warriorwomenblog@gmail.com>
2019-07-25 15:23:08 +0100

    Refactor strain with select and reject

--- ruby/strain/strain.rb
+++ ruby/strain/strain.rb
@@ -2,14 +2,10 @@ class Array
   # yield will be replaced by the contents of any block when the method is invoked
   # { |e| e < 10 }
   def keep
-    result = []
-    each { |e| result << e if (yield e) }
-    result
+    select { |e| e if (yield e) }
   end
 
   def discard
-    result = []
-    each { |e| result << e if (!yield e) }
-    result
+    reject { |e| e if (yield e) }
   end
 end
