579ec1e0e72d7abace0981752dba233fa3599c93
Angela Wolff <warriorwomenblog@gmail.com>
2019-07-25 15:17:47 +0100

    First iteration of strain using yield to pass block

--- ruby/strain/strain.rb
+++ ruby/strain/strain.rb
@@ -1,9 +1,15 @@
 class Array
+  # yield will be replaced by the contents of any block when the method is invoked
+  # { |e| e < 10 }
   def keep
-    []
+    result = []
+    each { |e| result << e if (yield e) }
+    result
   end
 
   def discard
-    []
+    result = []
+    each { |e| result << e if (!yield e) }
+    result
   end
 end
