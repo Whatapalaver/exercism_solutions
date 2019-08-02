e028ff086ca49cd6cd9f39600c14a371190886b6
whatapalaver <warriorwomenblog@gmail.com>
2019-08-02 13:54:41 +0100

    Scrabble refactor

--- ruby/scrabble-score/scrabble_score.rb
+++ ruby/scrabble-score/scrabble_score.rb
@@ -1,3 +1,18 @@
+module TILEABLE
+  TILES = Hash.new(0).merge(
+      'A' => 1, 'I' => 1, 'Q' => 10,
+      'B' => 3, 'J' => 8, 'R' => 1,
+      'C' => 3, 'K' => 5, 'S' => 1,
+      'D' => 2, 'L' => 1, 'T' => 1,
+      'E' => 1, 'M' => 3, 'U' => 1,
+      'F' => 4, 'N' => 1, 'V' => 4,
+      'G' => 2, 'O' => 1, 'W' => 4,
+      'H' => 4, 'P' => 3, 'X' => 8,
+      'Y' => 4,
+      'Z' => 10
+    )
+end
+
 class Scrabble
   TILES = { 'A' => 1, 'E' => 1, 'I' => 1, 'O' => 1, 'U' => 1,
             'L' => 1, 'N' => 1, 'R' => 1, 'S' => 1, 'T' => 1,
@@ -9,7 +24,7 @@ class Scrabble
             'Q' => 10, 'Z' => 10 }.freeze
   
   def initialize(word)
-    @word = word
+    @word_tiles = create_tiles(word)
   end
 
   def self.score(word)
@@ -17,12 +32,12 @@ class Scrabble
   end
 
   def score
-    create_tiles.sum { |value| TILES[value] }
+    @word_tiles.sum { |value| TILES[value] }
   end
 
   private
 
-  def create_tiles
-    @word.to_s.upcase.scan(/[A-Z]/)
+  def create_tiles(word)
+    word.to_s.upcase.scan(/[#{TILES.keys}]/)
   end
 end
