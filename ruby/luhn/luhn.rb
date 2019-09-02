f5ff533a665d07b3ab227704c30a9c266a8cfffa
whatapalaver <warriorwomenblog@gmail.com>
2019-09-02 16:56:39 +0100

    Refactor luhn

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,28 +1,29 @@
 class Luhn
-  attr_reader :card_number, :card_numbers
-
-  def initialize(card_number)
-    @card_number = card_number.delete(' ')
-    @card_numbers = @card_number.chars.map(&:to_i)
-  end
 
   def self.valid?(number)
     new(number).valid?
+  end
+
+  def initialize(card_number)
+    @card_number = card_number.delete(' ')
   end
 
   def valid?
     valid_input? && valid_luhn_sum?
   end
 
+  private
+
+  attr_reader :card_number
+
   def valid_input?
     card_number !~ /\D/ && card_number.length > 1
   end
 
-  private
-
   def luhn_algorithm
-    card_numbers
-      .reverse
+    card_number
+      .to_i
+      .digits
       .each_slice(2)
       .sum do |digit1, digit2 = 0|
         digit1 + luhn_doubling(digit2)
@@ -35,6 +36,6 @@ class Luhn
 
   def luhn_doubling(integer)
     product = integer * 2
-    product > 9 ? product - 9 : product
+    product < 10 ? product : product - 9
   end
 end
