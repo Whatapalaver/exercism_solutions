af81f4cfad8eba9f51a16e5e5d2f97e384d2f8d8
whatapalaver <warriorwomenblog@gmail.com>
2020-01-15 15:55:07 +0000

    Re-implementing hash and eql? methods

--- ruby/clock/clock.rb
+++ ruby/clock/clock.rb
@@ -1,36 +1,51 @@
 class Clock
   DAY_MINUTES = 1440
+  HOUR_MINUTES = 60
   attr_accessor :total_minutes
 
   def initialize(hour: 0, minute: 0)
-    @total_minutes = (hour * 60 + minute) % DAY_MINUTES
+    @total_minutes = (hour * HOUR_MINUTES + minute) % DAY_MINUTES
   end
 
   def to_s
-    format('%02d:%02d', hour, minute)
+    hours, minutes = total_minutes.divmod(HOUR_MINUTES)
+    format('%02d:%02d', hours, minutes)
   end
 
-  def +(other_clock)
-    # total_minutes += other_clock.total_minutes
-    Clock.new(minute: total_minutes + other_clock.total_minutes)
+  def +(other)
+    Clock.new(minute: total_minutes + other.total_minutes)
   end
 
-  def -(other_clock)
-    Clock.new(minute: total_minutes - other_clock.total_minutes)
+  def -(other)
+    Clock.new(minute: total_minutes - other.total_minutes)
   end
 
-  def ==(other_clock)
-    total_minutes == other_clock.total_minutes
+  def ==(other)
+    total_minutes == other.total_minutes
   end
 
-  private
-
-  def hour
-    total_minutes / 60 % 24
+  def eql?(other)
+    self.class == other.class && total_minutes == other.total_minutes
   end
 
-  def minute
-    total_minutes % 60
+  def hash
+    total_minutes.hash
   end
 
+=begin
+  # Re-implementing hash and eql?
+  # See https://stackoverflow.com/a/54961907/3258059
+
+  # irb test code
+
+  require './clock.rb'
+  c1 = Clock.new(minute: 10)
+  c2 = Clock.new(minute: 10)
+  c1 == c2
+  c1.eql?(c2)
+  clock_hash = Hash.new
+  clock_hash[c1] = '10 mins'
+  clock_hash[c2] = '10 mins'
+  clock_hash
+=end
 end
