4a9cf994eee1a2e9e1d40216ab51f2fda38c2ff1
whatapalaver <warriorwomenblog@gmail.com>
2020-01-21 10:33:20 +0000

    python change

--- python/two-fer/two_fer.py
+++ python/two-fer/two_fer.py
@@ -1,6 +1,2 @@
 def two_fer(name='you'):
-<<<<<<< HEAD
     return f'One for {name}, one for me.'
-=======
-    return 'One for %s, one for me.' % (name)
->>>>>>> Python solution complete and new Ruby challenge setup
