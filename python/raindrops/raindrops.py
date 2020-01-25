59b35d00b5da578bd440d74ec1ebb6d1fbf25ee8
Angela Wolff <warriorwomenblog@gmail.com>
2020-01-25 14:08:23 +0000

    Adding next python challenge

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,7 +1,20 @@
-raindrop = {3: 'Pling', 5: 'Plang', 7: 'Plong'}
+raindrop = {
+    3: 'Pling',
+    5: 'Plang',
+    7: 'Plong'
+}
 
 
 def convert(number: int) -> str:
     output_list = [raindrop[key] for key in raindrop if number % key == 0]
     output = "".join(output_list)
-    return str(number) if output == '' else output
+    return str(output or number)
+
+
+# def convert(number: int) -> str:
+#     result = "".join(raindrop[key] for key in raindrop if number % key == 0)
+#     return str(result or number)
+
+
+# print(convert(52))
+# print(convert(1))
