7774694ff44ea517b9a0595014b2121b1f9fbe5d
whatapalaver <warriorwomenblog@gmail.com>
2020-01-27 14:35:48 +0000

    High Scores solution

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,10 +1,17 @@
 def latest(scores):
-    pass
+    return scores[-1]
 
 
 def personal_best(scores):
-    pass
+    return max(scores)
 
 
 def personal_top_three(scores):
-    pass
+    sorted_copy = sorted(scores, reverse=True)
+    return sorted_copy[:3]
+
+
+# personal_top_three([10, 30, 90, 30, 100, 20, 10, 0, 30, 40, 40, 70, 70])
+# personal_top_three([40, 20, 40, 30])
+# personal_top_three([30, 70])
+# personal_top_three([30])
