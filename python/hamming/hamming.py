11d0bad1d3f20748a8bc1eed021cbe889cfe0819
Angela Wolff <warriorwomenblog@gmail.com>
2021-06-10 08:07:06 +0100

    New solutions and fresh challenges

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,11 +1,5 @@
 def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError("Both strands must have the same length")
-    a = [char for char in strand_a]
-    b = [char for char in strand_b]
-
-    accumulator = 0
-    for index, char in enumerate(a, start = 0):
-        if char != b[index]:
-            accumulator += 1
-    return accumulator
+    return sum(ch1 != ch2 for ch1, ch2 in zip(strand_a, strand_b))
+    
